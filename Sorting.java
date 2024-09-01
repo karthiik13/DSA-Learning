d9cf55bf2f409ab36b404e07420dc6e6e7dc0c45
karthiik13 <karthiksudarsan@gmail.com>
2024-09-01 13:02:42 +0530

    add bubbleSort algorithm

--- Sorting.java
+++ Sorting.java
@@ -22,6 +22,22 @@ public class Sorting {
         System.out.println();
     }
 
+    public static void bubbleSort(int arr[],int n){
+        for(int i=n-1;i>=1;i--){
+            for(int j=0;j<=i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        System.out.println("After Bubble sort:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
     public static void main(String args[]) {
 
         int [] arr = new int [50] ; 
@@ -35,7 +51,8 @@ public class Sorting {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
-        selection_sort(arr, n);
+        //selection_sort(arr, n);
+        bubbleSort(arr, n);
         sc.close();
     }
 }
